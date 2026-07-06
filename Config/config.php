278669ecde2d3ce60e9e5238c08719ac38eecf76
Alexander Zlobin <alexander.zlobin@m1.rs>
2026-07-06 15:29:33 +0200

    Use sales-snap footer domain

--- Config/config.php
+++ Config/config.php
@@ -45,7 +45,7 @@ return [
     'name'         => 'Zender',
     'description'  => 'This plugin replaces the SMS channel and allows you to send messages to WhatsApp using a Zender account. Intended for Mautic 5/6/7',
     'author'       => 'AlexanderZlobinM1',
-    'version'      => '1.2.14',
+    'version'      => '1.2.15',
     'release_date' => '2026-07-06',
     'license'      => 'GNU/GPLv3',
     'homepage'     => 'https://github.com/AlexanderZlobinM1/MauticZenderPlugin',
